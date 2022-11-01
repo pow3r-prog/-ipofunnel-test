c3d9bb249d403904bd13173978e5ecc1923ac75f
Володя Андрейчук <pow3r@Volodas-MacBook-Air.local>
2022-11-01 13:13:41 +0200

    update welcome page

--- src/containers/Welcome/Welcome.tsx
+++ src/containers/Welcome/Welcome.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Welcome = (): React.ReactElement => {
-  return <div>Welcome!</div>
+  return (
+    <div style={{ color: '#ffffff', fontSize: '32px' }}>
+      Welcome! Use navigation!
+    </div>
+  )
 }
 
 export default Welcome as React.FC
